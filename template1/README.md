1c5012405f5d73ddfced6eac94922534db208439
cdr <cdr@iteratec.de>
2017-09-15 16:14:21 -0300

    add template page

---
+++ template1/README.md
@@ -0,0 +1,24 @@
+
+MEAN<sup>2</sup> is based on the next technologies  
+* [Angular4 web starter](https://github.com/AngularClass/angular-starter)
+* [MongoDB](https://www.mongodb.com)
+* [Express](https://expressjs.com/)
+* Innograph (uses GraphQL for schema standartization)
+* [Bit](https://bitsrc.io/) - Manages js components, services and schemas
+
+If you're looking for the classic angular-1.x version of mean it has moved to this [1.x branch](https://github.com/linnovate/mean/tree/1.x) 
+
+## Prerequisites
+
+* Git
+* MongoDB
+* Node 6.x
+* npm 3.x ( or yarn)
+
+## Installation
+
+```
+git clone --depth 1 https://github.com/linnovate/mean.git  
+npm install  
+npm start  
+```
---
+++ template1/src/app/app.component.ts
@@ -0,0 +1,56 @@
+/**
+ * Angular 2 decorators and services
+ */
+import {
+  Component,
+  OnInit,
+  ViewEncapsulation
+} from '@angular/core';
+import { AppState } from './app.service';
+import { PostsService } from './posts/posts.service';
+/**
+ * App Component
+ * Top Level Component
+ */
+@Component({
+  selector: 'app',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: [
+    './app.component.scss'
+  ],
+  template: `
+    <header>
+    <md-toolbar color="primary">
+     <a [routerLink]="['/']" class="logotTxt">MEAN</a>
+   <a class="links" [routerLink]="['/posts']">Posts</a>
+     <span class="links">more...</span>
+    </md-toolbar>
+    </header>
+      <router-outlet></router-outlet>
+    <footer>
+    </footer>
+  `,
+  providers: [PostsService]
+})
+export class AppComponent implements OnInit {
+  public angularclassLogo = 'assets/img/angularclass-avatar.png';
+  public name = 'Angular 2 Webpack Starter';
+  public url = 'https://twitter.com/AngularClass';
+
+  constructor(
+    public appState: AppState
+  ) { }
+
+  public ngOnInit() {
+    console.log('Initial App State', this.appState.state);
+  }
+
+}
+
+/**
+ * Please review the https://github.com/AngularClass/angular2-examples/ repo for
+ * more angular app examples that you may copy/paste
+ * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
+ * For help or questions please contact us at @AngularClass on twitter
+ * or our chat on Slack at https://AngularClass.com/slack-join
+ */
